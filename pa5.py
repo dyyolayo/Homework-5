45fa86c5f6a0a0cd3f08b3c085593e9b47e756da
Work <user2@Oladayos-MacBook-Pro.local>
2024-05-08 21:49:48 -0500

    second

--- pa5.py
+++ pa5.py
@@ -7,8 +7,9 @@ Created on Wed May  8 18:23:10 2024
 """
 
 
-def gcd(inta,intb):
+def gcd(inta, intb):
     """function created to take the greatest common divisor of two integers"""
+
     if inta == 0:
         return intb
     if intb == 0:
@@ -24,33 +25,36 @@ def remove_pairs(direct):
 
     pairs = ["EW", "WE", "NS", "SN"]
 
-    for a in range(len(direct) - 1):
-        if direct[a : a + 2] in pairs:
-            new = direct[:a] + direct[a+ 2:]
-            return new
-    return direct
+    if len(direct) < 2:
+        return direct
+
+    if direct[:2] in pairs:
+        return remove_pairs(direct[2:])
+
+    return direct[0] + remove_pairs(direct[1:])
 
 
-def bisection_root(func, x1, x2):
+def bisection_root(func, num1, num2):
+    """function that applies the Bisection method given a function
+    and two numbers"""
 
-    f1 = func(x1)
-    f2 = func(x2)
+    fun1 = func(num1)
+    fun2 = func(num2)
     close = 10 ** -7
-    
-    if (f1 > 0 and f2 < 0) or (f1 < 0 and f2 > 0):
+
+    if (fun1 > 0 and fun2 < 0) or (fun1 < 0 and fun2 > 0):
         pass
     else:
         raise ValueError("Initial guesses do not bracket the root.")
-    
-    if abs(f1) < close:
-        return x1
-    if abs(f2) < close:
-        return x2
 
-    midp = (x1 + x2) / 2
-    f3 = func(midp)
-    
-    if (f3 > 0 and f1 < 0) or (f3 < 0 and f1 > 0):
-        return bisection_root(func, x1, midp)
-    else:
-        return bisection_root(func, midp, x2)
+    if abs(fun1) < close:
+        return num1
+    if abs(fun2) < close:
+        return num2
+
+    midp = (num1 + num2) / 2
+    fun3 = func(midp)
+
+    if (fun3 > 0 and fun1 < 0) or (fun3 < 0 and fun1 > 0):
+        return bisection_root(func, num1, midp)
+    return bisection_root(func, midp, num2)
